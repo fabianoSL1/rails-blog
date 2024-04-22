7bfe48e4d472e54ce32363b7db487b0a08c7dc4f
fabiano lima <fabianosl.dev@gmail.com>
2024-04-22 01:02:02 -0300

    fix deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,20 +76,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: ENV.fetch("SMTP_EMAIL")}
+  config.action_mailer.default_options = {from: ENV["SMTP_EMAIL"]}
 
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: ENV.fetch("SMTP_DOMAIN"),
-    user_name: ENV.fetch("SMTP_EMAIL"),
-    password: ENV.fetch("SMTP_PASSWORD"),
+    domain: ENV["SMTP_DOMAIN"],
+    user_name: ENV["SMTP_EMAIL"],
+    password: ENV["SMTP_PASSWORD"],
     authentication: 'plain',
     enable_starttls_auto: true
   }
 
-  Rails.application.routes.default_url_options[:host] = ENV.fetch("HOST")
+  Rails.application.routes.default_url_options[:host] = ENV["HOST"]
 
 end
